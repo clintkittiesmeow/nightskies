c15bb9019669640b8a989b5b664e74ea1a38c898
clintkittiesmeow <clintkittiesmeow@outlook.com>
2022-09-05 15:35:57 -0400

    Initial tutorial build.  Added CSS.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nightskies',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='clintkittiesmeow',
+    author_email='',
+    description=''
+)
